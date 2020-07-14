ce8be9649593a3b3907e08b8cc8982f00611d213
VimIcewind <VimIcewind@gmail.com>
2020-07-14 10:41:06 +0800

    Stack OutOfMemoryError

---
+++ src/main/java/ch02/JavaVMStackOOM.java
@@ -0,0 +1,31 @@
+package ch02;
+
+/**
+ * VM Args: -Xss2M
+ * @author HWD
+ * @date 2020/7/14 10:11
+ */
+public class JavaVMStackOOM {
+    private void dontStop() {
+        while (true) {
+
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dontStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
