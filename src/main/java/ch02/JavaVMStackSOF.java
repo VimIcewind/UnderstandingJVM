a8a3f75fdd60408fdb9fccc5b7bb54507a52965a
VimIcewind <VimIcewind@gmail.com>
2020-07-14 09:53:52 +0800

    Stack StackOverflowError

---
+++ src/main/java/ch02/JavaVMStackSOF.java
@@ -0,0 +1,25 @@
+package ch02;
+
+/**
+ * VM Args: -Xss128k
+ * @author HWD
+ * @date 2020/7/14 09:34
+ */
+public class JavaVMStackSOF {
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) throws Throwable {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length: " + oom.stackLength);
+            throw e;
+        }
+    }
+}
