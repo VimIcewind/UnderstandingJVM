c8ccc4bef339b9c9b40738f128ed0ea68bba4b73
VimIcewind <VimIcewind@gmail.com>
2020-07-16 15:01:42 +0800

    Runtime Constant Poo OutOfMemoryError: Metaspace

--- src/main/java/ch02/RuntimeConstantPoolOOM.java
+++ src/main/java/ch02/RuntimeConstantPoolOOM.java
@@ -3,7 +3,7 @@ package ch02;
 import java.util.*;
 
 /**
- * VM Args: -XX:PermSize=10M -XX:MaxPermSize=10M
+ * VM Args: -XX:PermSize=6M -XX:MaxPermSize=6M
  * -XX:PermSize=10M -XX:MaxPermSize=10M support was removed in 8.0
  * -Xmx6M -XX:MetaspaceSize=10M -XX:MaxMetaspaceSize=10M
  * @author HWD
@@ -11,13 +11,12 @@ import java.util.*;
  */
 public class RuntimeConstantPoolOOM {
     public static void main(String[] args) {
-        // 使用 List 保持着常量池引用，避免 Full GC 回收常量池行为
-        List<String> list = new ArrayList<String>();
-        // 10MB 的 PermSize 在 integer 范围内足够产生 OOM 了
-        int i = 0;
+        // 使用 Set 保持着常量池引用，避免 Full GC 回收常量池行为
+        Set<String> set = new HashSet<String>();
+        // 在 short 范围内足以让 6MB 的 PermSize 产生OOM了
+        short i = 0;
         while (true) {
-            System.out.println(i);
-            list.add(String.valueOf(i++).intern());
+            set.add(String.valueOf(i++).intern());
         }
     }
 }
